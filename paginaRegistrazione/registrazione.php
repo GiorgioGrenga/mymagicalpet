1be04dfabf37755959fc5847b691428eebfaecb4
GiorgioGrenga <83449504+GiorgioGrenga@users.noreply.github.com>
2021-05-11 19:11:46 +0200

    registrazione

--- paginaRegistrazione/registrazione.php
+++ paginaRegistrazione/registrazione.php
@@ -1,18 +1,14 @@
 <!-- la registrazione, per la natura del localhost, funziona solo sul pc dove risiede il server-->
-
-<?php
-
-session_start();
-
-// variabili connessione
-
-$host="localhost";
-$user="root";
-$password="";
-
-$con = mysqli_connect($host,$user,$password);  // funzione connessione
-
-mysqli_select_db($con,'elenco_utenti');   // seleziona il database
+<html>
+<title>Esempio in PHP</title>
+<head></head>
+<body>
+    <?php
+	
+        $dbconn =pg_connect("host=localhost port=5432 
+            dbname=elenco_iscritti
+             user=postgres password=Giorgio99!")
+        or die('Could not connect: ' .pg_last_error());
 
 // variabili di tipo post che prendono in input i dati dalla form registrazione.html
 
@@ -22,12 +18,11 @@ $nome=$_POST['nome'];
 $cognome=$_POST['cognome'];
 $uname=$_POST['username'];
 
-$sql="select * from utenti where username='$uname'";  // fa il check con username ( chiave primaria della tabella ) all'interno del database 
+$sql="select * from iscritti where username='$uname'";  // fa il check con username ( chiave primaria della tabella ) all'interno del database 
+$result=pg_query($sql)or die('Query failed:'.pg_last_error());
 
-	
-$result=mysqli_query($con, $sql);
 
-$num = mysqli_num_rows($result);
+$num = pg_num_rows($result);
 
 // controlla se l'utente esiste già nel database 
 
@@ -37,13 +32,16 @@ if($num == 1){   // se esiste una riga
 	header('location:reg_fallita.html');  // reinderizzato nella pagina reg fallita
 	
 }
-else{  // altrimenti lo inserisce nella tabella utenti tramite la insert
-	$reg= "insert into utenti(email,password,nome,cognome,username) values ('$email', '$password','$nome','$cognome','$uname') ";
-	mysqli_query($con, $reg);
+else{  // altrimenti lo inserisce nella tabella iscritti tramite la insert
+	$reg= "insert into iscritti(email,password,nome,cognome,username) values ('$email', '$password','$nome','$cognome','$uname') ";
+	pg_query($dbconn, $reg);
 	echo"Registrazione effettuata.";
 	header('location:reg_successo.html');
 	
 }
-
-?>
-
+//chiusura
+        pg_free_result($result);
+        pg_close($dbconn);
+        ?>
+    </body>
+</html>
